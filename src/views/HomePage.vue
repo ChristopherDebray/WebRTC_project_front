f03d4bf5d85a13930704d52a84da82bc3efaaf18
Christopher Debray <christopherdebray1@gmail.com>
2025-02-14 15:32:50 +0100

    feat: Added socket testing

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,6 +2,18 @@
 import { io } from 'socket.io-client';
 import VideoManager from '@/components/medias/VideoManager.vue'
 const socket = io(import.meta.env.VITE_WEBSOCKET_SERVER);
+
+socket.on('connect', () => {
+    console.log('connected to socket server');
+})
+socket.emit('test', {
+    firstName: 'Chris',
+    lastName: 'Deb',
+})
+
+socket.on("testReceived", () => {
+    console.log('web socket test received');
+})
 </script>
 
 <template>
