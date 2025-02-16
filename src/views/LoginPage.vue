e8a8a91b172794f6eef7106455534e2884a07d4f
Christopher Debray <christopherdebray1@gmail.com>
2025-02-16 11:18:15 +0100

    feat: Added visual for login form

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+// Define reactive state
+const firstName = ref('');
+const password = ref('password');
+
+// Validation rules
+const firstNameRules = [
+    (value: string) => value?.length >= 3 || 'First name must be at least 3 characters.'
+];
+
+const passwordRules = [
+    (value: string) => value?.length >= 3 || 'Password must be at least 3 characters.'
+];
+</script>
+
+<template>
+    <v-sheet class="mx-auto px-8 py-8" width="350" elevation="16">
+        <v-form fast-fail @submit.prevent>
+            <v-text-field prepend-icon="mdi-account" v-model="firstName" :rules="firstNameRules" label="First name"
+                class="mb-3"></v-text-field>
+            <v-text-field prepend-icon="mdi-lock" v-model="password" :rules="passwordRules"
+                label="Password"></v-text-field>
+            <v-btn class="mt-3" type="submit" block color="success">Login</v-btn>
+        </v-form>
+    </v-sheet>
+</template>
+
+
+<style scoped></style>
