fab82c5ab2b3e2077ff9eb6cfa98070ab9f37c4a
Christopher Debray <christopherdebray1@gmail.com>
2025-02-18 12:13:05 +0100

    feat: Added CallingUser component

---
+++ src/components/calls/CallingUser.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useSocketStore } from '@/stores/socket';
+
+const socketStore = useSocketStore();
+</script>
+
+<template>
+    <div>
+        <div class="bg-grey-lighten-3 px-8 py-2 rounded-lg elevation-8">
+            <div>
+                Calling...
+            </div>
+            <div class="d-flex">
+                <v-avatar :color="socketStore.calledUser?.userColor" size="28">
+                    {{ socketStore.calledUser?.userInitials }}
+                </v-avatar>
+                <div class="pl-4">
+                    {{ socketStore.calledUser?.userName }}
+                </div>
+            </div>
+            <div class="d-flex justify-space-around pt-4">
+                <v-btn class="ma-2" color="red" size="small" icon="mdi-phone-hangup"
+                    @click.once="socketStore.rejectCall"></v-btn>
+            </div>
+        </div>
+    </div>
+</template>
